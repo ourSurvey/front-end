cefbb0729cb10e92ab372cd32583064333e52f59
jungsoo <sunpl13@naver.com>
2022-07-24 17:08:28 +0900

    OS-2 공통 스타일 정의

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`, // prependData 옵션 추가, 모든 파일에서 사용 가능하도록 설정
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

59867b0236e75f6f74b07af8c774bf83d851b918
jungsoo <sunpl13@naver.com>
2022-07-24 19:00:39 +0900

    테스트 코드 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "styles/Home.module.scss";
+import { Button } from "components/Button";
 
 const Home: NextPage = () => {
   return (
@@ -20,6 +21,7 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
+        <Button color="#0066D9" btnText="다음" textColor="#fff" />
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
--- tests/index/index.test.tsx
+++ tests/index/index.test.tsx
@@ -1,16 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../../pages/index'
+import { render, screen } from "@testing-library/react";
+import Home from "../../pages/index";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    const { container } = render(<Home />)
+describe("Home", () => {
+  it("renders a heading", () => {
+    const { container } = render(<Home />);
 
-    const heading = screen.getByRole('heading', {
+    const heading = screen.getByRole("heading", {
       name: /welcome to next\.js!/i,
-    })
+    });
 
-    expect(heading).toBeInTheDocument()
+    expect(heading).toBeInTheDocument();
 
-    expect(container).toMatchSnapshot()
-  })
-})
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders a button", () => {
+    const { container } = render(<Home />);
+
+    const heading = screen.getByText("다음");
+
+    expect(heading).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+});

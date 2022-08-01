b6e20470d9a62c5fffbd7b65155d612735654b54
jungsoo <sunpl13@naver.com>
2022-08-01 16:16:58 +0900

    OS-3 불필요 의존성 제거

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`, // prependData 옵션 추가, 모든 파일에서 사용 가능하도록 설정
-  },
   env: {
     NEXT_PUBLIC_API: "3.35.216.132",
   },
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,17 +1,8 @@
 import type { NextPage } from "next";
-import css from "styled-jsx/css";
 import Register from "components/Register";
-import styles from "styles/Home.module.scss";
 const Index: NextPage = () => {
-  const style = css`
-    .container {
-      width: 100vw;
-      height: 100vh;
-      padding: 21.5px 20px 35px 20px;
-    }
-  `;
   return (
-    <div className={styles.container}>
+    <div>
       <Register />
     </div>
   );

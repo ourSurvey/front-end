eb1b1a489cc17fc8bb1192d111e1577935e388de
jungsoo <sunpl13@naver.com>
2022-08-01 15:45:39 +0900

    OS-3 체크박스 모듈 CSS 이모션으로 변경

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import checkbox from "styles/components/CheckBox.module.scss";
+import { useState } from "react";
+import styled from "@emotion/styled";
+import { Common, Pretendard } from "styles/common";
 interface IProps {
   text: string;
   checked: boolean;
@@ -10,13 +11,54 @@ const CheckBox = ({ text, checked, disabled = false }: IProps) => {
   const defaultChecked = checked ? checked : false;
   const [isChecked, setIsChecked] = useState(defaultChecked);
   return (
-    <div className={checkbox.checkboxWrapper}>
+    <CheckboxWrapper>
       <label>
         <input type="checkbox" checked={isChecked} onChange={() => setIsChecked((prev) => !prev)} disabled={disabled} />
-        <span>{text}</span>
+        <Span>{text}</Span>
       </label>
-    </div>
+    </CheckboxWrapper>
   );
 };
 
 export default CheckBox;
+
+const CheckboxWrapper = styled.div`
+  position: relative;
+  & input[type="checkbox"] {
+    -webkit-appearance: none;
+    appearance: none;
+    /* creating a custom design */
+    width: 1.375rem;
+    height: 1.375rem;
+    border-radius: 0.375rem;
+    background-image: url("images/check.svg");
+    background-size: 50% 50%;
+    background-position: 50%;
+    background-repeat: no-repeat;
+    border: 1px solid #c2c5d0;
+    outline: none;
+    cursor: pointer;
+
+    &:checked {
+      background-image: url("images/checkedCheck.svg");
+      background-size: 50% 50%;
+      background-position: 50%;
+      background-repeat: no-repeat;
+    }
+  }
+  & input[type="checkbox"]:disabled {
+    border-color: ${Common.colors.GY100};
+    background-color: ${Common.colors.GY100};
+  }
+  & input[type="checkbox"]:disabled + span {
+    color: ${Common.colors.GY100};
+  }
+`;
+
+const Span = styled.span`
+  ${Pretendard({ font: 1.4, weight: 400, color: Common.colors.GY900 })}
+  position: absolute;
+  margin-left: 6px;
+  top: -3px;
+  cursor: pointer;
+`;

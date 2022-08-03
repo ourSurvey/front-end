33a0ef9735a6e65e507543b2519422e7976099b4
jungsoo <sunpl13@naver.com>
2022-08-04 01:11:55 +0900

    홈 컴포넌트 테스트 수정

--- tests/index/HearderName.test.tsx
+++ tests/index/HearderName.test.tsx
@@ -3,10 +3,16 @@ import HeaderName from "components/HeaderName";
 import { render, screen } from "@testing-library/react";
 
 describe("헤더 테스트", () => {
-  const testName: string = "회원가입";
   it("헤더에는 prop로 전달한 이름이 출력되어야 한다", () => {
     render(<HeaderName name="회원가입" hasBack={true} hasNext={true} />);
     const text = screen.getByText("회원가입");
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it("헤더에는 prop로 전달한 이름이 출력되어야 한다", () => {
+    render(<HeaderName name="로그인" hasBack={true} hasNext={true} />);
+    const text = screen.getByText("로그인");
 
     expect(text).toBeInTheDocument();
   });
--- tests/index/index.test.tsx
+++ tests/index/index.test.tsx
@@ -5,7 +5,7 @@ describe("Home", () => {
   it("renders a button", () => {
     const { container } = render(<Home />);
 
-    const heading = screen.getByText("이메일");
+    const heading = screen.getByText("홈 페이지 입니다");
 
     expect(heading).toBeInTheDocument();
     expect(container).toMatchSnapshot();

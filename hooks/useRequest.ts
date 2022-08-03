64e2a468a79dfcc609fbb3e982eaa54c0f79a4bc
jungsoo <sunpl13@naver.com>
2022-08-04 01:07:27 +0900

    useRequest Hook 추가

---
+++ hooks/useRequest.ts
@@ -0,0 +1,3 @@
+import { useQuery, QueryKey, UseQueryOptions, QueryFunction } from "react-query";
+
+export const useRequest = (request: QueryKey, func: QueryFunction, options?: UseQueryOptions) => useQuery(request, func, options);

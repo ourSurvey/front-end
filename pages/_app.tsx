f523137995205882e95aa34873d6e3a689d57a17
jungsoo <sunpl13@naver.com>
2022-08-08 17:38:54 +0900

    recoil 전역 설정 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,28 @@
+/** @jsxImportSource @emotion/react */
+import { global } from "styles/global";
 import { Global } from "@emotion/react";
+import { RecoilRoot } from "recoil";
 import { useState } from "react";
 import type { AppProps } from "next/app";
-import { global } from "styles/global";
 import Layout from "components/Layout";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import TokenProvider from "services/TokenProvider";
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
-  const router = useRouter();
-
-  useEffect(() => {
-    const moveLoginPage = (): void => {
-      if (!TokenProvider.has("accessToken")) {
-        router.push("/login");
-      }
-    };
-    moveLoginPage();
-  }, []);
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Layout>
-          <>
-            <Global styles={global} />
-            <Component {...pageProps} />
-          </>
-        </Layout>
-      </Hydrate>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout>
+            <>
+              <Global styles={global} />
+              <Component {...pageProps} />
+            </>
+          </Layout>
+        </Hydrate>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
 

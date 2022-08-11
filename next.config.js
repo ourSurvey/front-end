6c4ecc12cfeb9847e3a4c9e5800ed536ff26df59
jungsoo <sunpl13@naver.com>
2022-08-11 20:06:21 +0900

    env 파일 추가

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    NEXT_PUBLIC_API: "http://3.35.216.132",
-  },
   webpack: (config) => {
     // 아래를 추가합니다.
     config.module.rules.push({

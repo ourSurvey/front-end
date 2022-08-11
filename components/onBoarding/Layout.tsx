13b860bbc28ca94eab542f2b9a55b9d1b750a09c
jungsoo <sunpl13@naver.com>
2022-08-12 00:56:09 +0900

    불필요 의존성 삭제

--- components/onBoarding/Layout.tsx
+++ components/onBoarding/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "@emotion/styled";
 import { Common, Pretendard } from "styles/common";
 import dynamic from "next/dynamic";

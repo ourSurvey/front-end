dc493963d2abcbb33568fd51583abfade93d6652
jungsoo <sunpl13@naver.com>
2022-08-17 21:51:28 +0900

    서베이 관련 api class 추가

---
+++ services/survey.service.ts
@@ -0,0 +1,16 @@
+class SurveyService {
+  async surveyDetail(id: number | string, token: string) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/api/survey/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json().then((data) => ({ data }));
+  }
+}
+
+export default new SurveyService();

5befc49ddd4c4198125859d3cb45208831fa5204
jungsoo <sunpl13@naver.com>
2022-08-25 21:55:49 +0900

    설문 개요 생성 css 추가

--- pages/survey/create/index.tsx
+++ pages/survey/create/index.tsx
@@ -3,7 +3,23 @@ import styled from "@emotion/styled";
 import { Common, Pretendard } from "styles/common";
 import OutLineButton from "components/common/OutLineButton";
 import { Button } from "components/common/Button";
+import React, { useEffect, useState } from "react";
 const Index = () => {
+  const [content, setContent] = useState("");
+  const [visibleSpan, setvisibleSpan] = useState(1);
+  const [title, setTitle] = useState("");
+  const Title = styled.span`
+    opacity: ${visibleSpan};
+  `;
+
+  useEffect(() => {
+    if (content !== "") {
+      setvisibleSpan(0);
+    } else {
+      setvisibleSpan(1);
+    }
+  }, [content]);
+
   return (
     <Summary>
       <header>
@@ -11,8 +27,15 @@ const Index = () => {
       </header>
 
       <form>
-        <input type="text" placeholder="제목" />
-        <textarea placeholder="설문에 대한 설명을 적어주세요" name="survey-summary" id="" cols={30} rows={10}></textarea>
+        <input type="text" placeholder="제목" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)} />
+        <TextAreaContainder>
+          <Title>설문에 대한 설명을 적어주세요</Title>
+          <textarea
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)}
+            placeholder="&#13;&#10;ex) 모집 계기, 참여시 리워드 등"
+            name="survey-summary"
+          ></textarea>
+        </TextAreaContainder>
         <ButtonContainer>
           <OutLineButton isDisabled={false} textColor={Common.colors.GY900} btnText="임시저장" borderColor={Common.colors.GY900} />
           <Button isDisabled={false} textColor="#fff" btnText="다음" color={Common.colors.BL500} />
@@ -26,9 +49,28 @@ export default Index;
 
 const Summary = styled.div`
   position: relative;
-
+  height: 100%;
   & header {
     margin-bottom: 28px;
+  }
+  & form {
+    display: flex;
+    flex-direction: column;
+    height: calc(100% - 114px);
+  }
+
+  & input {
+    width: 100%;
+    height: 33px;
+    padding: 5px 10px;
+    border: none;
+    border-bottom: 1px solid ${Common.colors.GY100};
+    margin-bottom: 12px;
+  }
+  & ::placeholder {
+    ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY300 })}
+    line-height: 150%;
+    letter-spacing: -0.03em;
   }
 `;
 
@@ -44,8 +86,34 @@ const ButtonContainer = styled.div`
     font-size: 0.75rem;
     padding: 9px 15px;
     border-radius: 5px;
+    line-height: 150%;
+    width: auto;
+    letter-spacing: -0.03em;
   }
   & button:not(:last-child) {
     margin-right: 15px;
+  }
+`;
+
+const TextAreaContainder = styled.div`
+  position: relative;
+  border: none;
+  height: 100%;
+  flex: 1;
+  & span {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    ${Pretendard({ font: 1.3, weight: 400, color: Common.colors.GY500 })}
+    line-height: 150%;
+    letter-spacing: -0.03em;
+  }
+
+  & textarea {
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    overflow-y: scroll;
+    border: none;
   }
 `;

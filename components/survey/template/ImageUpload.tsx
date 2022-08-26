c9bdad6ea16818471e6fac19887caa28cfa43f16
jungsoo <sunpl13@naver.com>
2022-08-26 14:24:08 +0900

    객관식 image upload 컴포넌트

---
+++ components/survey/template/ImageUpload.tsx
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+import { Common } from "styles/common";
+import SlideArrow from "public/icon/slide-arrow.svg";
+import ImageUpLoadSvg from "public/icon/img.svg";
+import React from "react";
+
+const ImageUpload = () => {
+  return (
+    <MultipleSelectionLi>
+      <SlideArrow />
+      <InputContainer>
+        <input
+          type="file"
+          id="select-file"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => console.log(e.target.files[0])}
+          className="input-file"
+          accept="image/jpg, image/png, image/jpeg"
+        />
+        <label htmlFor="select-file">
+          <ImageUpLoadSvg />
+        </label>
+      </InputContainer>
+    </MultipleSelectionLi>
+  );
+};
+
+export default ImageUpload;
+
+const MultipleSelectionLi = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const InputContainer = styled.div`
+  position: relative;
+  flex: 1;
+  margin-left: 7px;
+
+  width: 100%;
+  height: 46px;
+  border: 1px solid ${Common.colors.GY300};
+  border-radius: 10px;
+  padding: 12px 15px;
+  & input[type="file"] {
+    position: absolute;
+    width: 0;
+    height: 0;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
+
+  & svg {
+    position: absolute;
+
+    right: 15px;
+    top: 13px;
+  }
+  & label {
+    display: inline-block;
+    font-size: inherit;
+    line-height: normal;
+    vertical-align: middle;
+    cursor: pointer;
+  }
+`;

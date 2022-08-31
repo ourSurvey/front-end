35ca9cf04616ed95cf047e1f6ed05b6883916d7b
jungsoo <sunpl13@naver.com>
2022-08-31 17:59:06 +0900

    레이아웃 함수 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,28 +2,33 @@
 import { global } from "styles/global";
 import { Global } from "@emotion/react";
 import { RecoilRoot } from "recoil";
-import { useState } from "react";
+import { ReactElement, ReactNode, useState } from "react";
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { NextPage } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const [queryClient] = useState(() => new QueryClient());
 
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Layout>
-            <>
-              <Global styles={global} />
-              <Component {...pageProps} />
-            </>
-          </Layout>
-        </Hydrate>
-      </QueryClientProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <Global styles={global} />
+          {getLayout(<Component {...pageProps} />)}
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
-
-export default MyApp;

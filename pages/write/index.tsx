956b188b23a6074d692a9f0c5469095b7dec9f1b
jungsoo <sunpl13@naver.com>
2022-09-02 18:23:40 +0900

    서베이 생성 페이지 UI 생성

--- pages/write/index.tsx
+++ pages/write/index.tsx
@@ -1,73 +1,75 @@
-import React from "react";
 import styled from "@emotion/styled";
 import { Common, Pretendard } from "styles/common";
-import QusetionTitle from "components/survey/template/QusetionTitle";
 import CreateSurveyHeader from "components/survey/CreateSurveyHeader";
-import InvertedTriangle from "public/icon/inverted-triangle.svg";
-import Question from "components/survey/template/Question";
-const Index = () => {
-  return (
-    <PartContainer>
-      <header>
-        <CreateSurveyHeader hasUnderLine={false} name="질문을 작성해주세요." step="02" />
-        <SubjectContainer>
-          <PartTitle>
-            <h1>PART 1</h1>
-            <span className="total-step">/3</span>
-          </PartTitle>
-          <QusetionCount>
-            <span>총 NN개 질문</span>
-            <InvertedTriangle />
-          </QusetionCount>
-        </SubjectContainer>
-      </header>
-      <QusetionTitle hasImageInput={false} />
-      <Line></Line>
+import Part from "components/survey/template/Part";
+import React, { useState, useEffect } from "react";
+import SubLayout from "components/SubLayout";
+import { ISurveyData, ISection } from "types/survey";
 
-      <Question />
-    </PartContainer>
+export default function Index() {
+  const [surveyData, setSurveyData] = useState<ISurveyData>({
+    id: 0,
+    subject: "",
+    content: "",
+    startDate: "",
+    endDate: "",
+    minute: 0,
+    openFl: 1,
+    tempFl: 1,
+    closingComment: "",
+    hashtag: [],
+    sections: [
+      {
+        title: "", //섹션제목,
+        content: "", //설명,
+        nextSection: -1, //*다음섹션(Integer|-1이면 이 섹션이 마지막 섹션, 사실 그냥 index값임 프론트에서도 설문 만들 때 정렬해서 보여줘야하니깐 index값 그대로 넣기),
+        questions: [
+          {
+            ask: "",
+            explain: "",
+            multiFl: 1,
+            essFl: 0,
+            dupFl: 0,
+            oder: 0,
+            questionItems: [{ content: "", oder: 0, nextSection: 0 }],
+          },
+        ],
+      },
+    ],
+  });
+
+  const PartSectionContainer = styled.div`
+    display: block;
+    height: calc(100% - 54px) !important;
+
+    & section:not(:last-child) {
+    }
+
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  `;
+
+  return (
+    <WriteContainer>
+      <CreateSurveyHeader hasUnderLine={false} name="질문을 작성해주세요." step="02" />
+      <PartSectionContainer id="section2">
+        {surveyData.sections.map((_, index: number) => {
+          return <Part PartNum={index} surveyData={surveyData} key={index} setSection={setSurveyData} />;
+        })}
+      </PartSectionContainer>
+    </WriteContainer>
   );
+}
+
+Index.getLayout = function getLayout(page: React.ReactElement) {
+  return <SubLayout>{page}</SubLayout>;
 };
 
-export default Index;
-
-const PartContainer = styled.section`
+const WriteContainer = styled.main`
+  width: 100%;
   height: 100%;
-`;
-
-const SubjectContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Line = styled.div`
-  width: calc(100% + 20 * 2);
-
-  margin: 0 -20px 0 -20px;
-  height: 10px;
+  background-clip: padding-box;
   background-color: ${Common.colors.GY50};
-`;
-
-const PartTitle = styled.div`
-  & h1 {
-    display: inline;
-    ${Pretendard({ font: 1.6, weight: 700, color: Common.colors.GR500 })};
-    line-height: 150%;
-    letter-spacing: -0.03em;
-  }
-  & .total-step {
-    ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY300 })};
-    line-height: 150%;
-  }
-`;
-
-const QusetionCount = styled.div`
-  & span {
-    ${Pretendard({ font: 1, weight: 700, color: Common.colors.GY700 })};
-    line-height: 150%;
-    letter-spacing: -0.03em;
-  }
-  & svg {
-    margin-left: 6px;
-  }
 `;

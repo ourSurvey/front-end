ec8443992fbba5c521969e57d696f600436536ac
jungsoo <sunpl13@naver.com>
2022-09-05 23:17:53 +0900

    [#9] : feat 부가정보 state 답변하지 않고 넘어가기 처리
    null 일 경우 데이터를 보내지 않기 위해 null인 프로퍼티 삭제

--- states/onBoard.ts
+++ states/onBoard.ts
@@ -1,12 +1,31 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 interface IonBoard {
-  gender: string | null;
+  gender: null | "M" | "F";
   age: number | null;
   tel: string | null;
+}
+
+interface LooseObject {
+  [key: string]: any;
 }
 
 export const addtionState = atom<IonBoard>({
   key: "addtionState",
   default: { gender: null, age: null, tel: null },
+});
+
+export const nullDeleteAdttionState = selector({
+  key: "nullDeleteAdttionState",
+  get: ({ get }) => {
+    const addtionData = get(addtionState);
+    const newObj: LooseObject = {};
+    for (let [key, value] of Object.entries(addtionData)) {
+      //값이 null이 아닌 값만 넣어서 리턴
+      if (value !== null) {
+        newObj[key] = value;
+      }
+    }
+    return newObj;
+  },
 });

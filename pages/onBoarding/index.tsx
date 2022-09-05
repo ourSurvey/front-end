0d82951513ecb15d2a20260e7d588e735e2a09cc
jungsoo <sunpl13@naver.com>
2022-09-06 08:45:57 +0900

    [#9] feat : 미사용 의존성 제거

--- pages/onBoarding/index.tsx
+++ pages/onBoarding/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Layout from "components/onBoarding/Layout";
 
-import { useEffect, useState } from "react";
 import SelectGengder from "components/onBoarding/SelectGengder";
 
 const index: NextPage = () => {

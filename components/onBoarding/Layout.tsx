437bcb44aed1a7ec209547abe746a9bd5cb9a9cf
jungsoo <sunpl13@naver.com>
2022-09-05 23:15:11 +0900

    [#9] feat : 온보딩 레이아웃 수정
    height가 짧은 디바이스 대응을 위한 스크롤 추가

--- components/onBoarding/Layout.tsx
+++ components/onBoarding/Layout.tsx
@@ -14,14 +14,16 @@ const Layout = (props: Props) => {
       <header>
         <StepProgress />
       </header>
-      <main>{props.children}</main>
-      <footer>
-        <UlStyle>
-          <li>답변하신 정보는 앞으로 참여하실 설문의 인구통계정보 수집 문항에 자동 입력됩니다.</li>
-          <li>입력하신 정보는 설문을 참여할 때마다 수정, 삭제가 가능합니다.</li>
-          <li>정보를 입력하지 않아도 서비스를 이용할 수 있습니다.</li>
-        </UlStyle>
-      </footer>
+      <div className="wrpper">
+        <main>{props.children}</main>
+        <footer>
+          <UlStyle>
+            <li>답변하신 정보는 앞으로 참여하실 설문의 인구통계정보 수집 문항에 자동 입력됩니다.</li>
+            <li>입력하신 정보는 설문을 참여할 때마다 수정, 삭제가 가능합니다.</li>
+            <li>정보를 입력하지 않아도 서비스를 이용할 수 있습니다.</li>
+          </UlStyle>
+        </footer>
+      </div>
     </OnbordingLayout>
   );
 };
@@ -40,6 +42,16 @@ const UlStyle = styled.ul`
 `;
 
 const OnbordingLayout = styled.div`
+  height: 100%;
+
+  & .wrpper {
+    height: calc(100% - 56px) !important;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
   & h1 {
     margin-top: 44px;
     margin-bottom: 0;

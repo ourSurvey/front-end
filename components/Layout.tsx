6dd795a00379b9d0f64173d96eacb98c174b8c28
jungsoo <sunpl13@naver.com>
2022-09-05 20:35:27 +0900

    [#9] feat : 레이아웃 모달 부분 portal로 전체 이동
    리렌더링 이슈로 인한 수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { useEffect } from "react";
 import styled from "@emotion/styled";
-import { css, keyframes } from "@emotion/react";
-import Alert from "./common/Alert";
-import { showToastState } from "states/modal";
 
-import { useRecoilValue } from "recoil";
+import Alert from "./common/Alert";
+
 import { useRouter } from "next/router";
 import NavBar from "./common/NavBar";
 import { hasNavbar } from "utills/hasNavbar";
@@ -20,8 +18,6 @@ const Layout = ({ children }: IProps) => {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   };
 
-  const visible = useRecoilValue(showToastState);
-
   const router = useRouter();
 
   useEffect(() => {
@@ -30,27 +26,6 @@ const Layout = ({ children }: IProps) => {
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-  const fadein = keyframes`
-  from {bottom: -35px; opacity: 0;}
-  to {bottom: 0; opacity: 1;}
-`;
-  const fadeout = keyframes`
-  from {bottom: 0; opacity: 1;}
-  to {bottom: -35px; opacity: 0;}
-`;
-  const toast = css`
-    position: absolute;
-    bottom: 0;
-    visibility: ${visible ? `visible` : "hidden"};
-    animation: ${visible ? fadein : fadeout} 0.2s ease-out;
-    transition: visibility 0.2s ease-out;
-    margin-bottom: ${hasNavbar(router.asPath) ? "27px" : ""}; //nav바가 있다면 알럿 위치 위로 올림
-    padding: 21.5px 20px 35px 20px;
-    left: 0;
-    right: 0;
-    z-index: 300;
-  `;
 
   const LayoutContainer = styled.div`
     position: relative;
@@ -62,9 +37,7 @@ const Layout = ({ children }: IProps) => {
     <LayoutContainer>
       {children}
       <Portal selector="#portal">
-        <div css={toast} role="alert">
-          <Alert />
-        </div>
+        <Alert />
       </Portal>
       {hasNavbar(router.asPath) ? <NavBar /> : ""}
     </LayoutContainer>
--- components/common/Alert.tsx
+++ components/common/Alert.tsx
@@ -5,9 +5,37 @@ import PinkExcalmationMark from "public/images/pinkExclamationMark.svg";
 import { Common, Pretendard } from "styles/common";
 import { toastState } from "states/modal";
 import { useRecoilState } from "recoil";
-
+import { css, keyframes } from "@emotion/react";
+import { showToastState } from "states/modal";
+import { useRecoilValue } from "recoil";
+import { hasNavbar } from "utills/hasNavbar";
+import { useRouter } from "next/router";
 const Alert = () => {
   const [ToastState, setToastState] = useRecoilState(toastState);
+  const visible = useRecoilValue(showToastState);
+  const router = useRouter();
+
+  const fadein = keyframes`
+  from {bottom: -35px; opacity: 0;}
+  to {bottom: 0; opacity: 1;}
+`;
+  const fadeout = keyframes`
+  from {bottom: 0; opacity: 1;}
+  to {bottom: -35px; opacity: 0;}
+`;
+
+  const toast = css`
+    position: absolute;
+    bottom: 0;
+    visibility: ${visible ? `visible` : "hidden"};
+    animation: ${visible ? fadein : fadeout} 0.2s ease-out;
+    transition: visibility 0.2s ease-out;
+    margin-bottom: ${hasNavbar(router.asPath) ? "27px" : ""}; //nav바가 있다면 알럿 위치 위로 올림
+    padding: 21.5px 20px 35px 20px;
+    left: 0;
+    right: 0;
+    z-index: 300;
+  `;
 
   useEffect(() => {
     if (ToastState.visible) {
@@ -22,7 +50,7 @@ const Alert = () => {
   }, [ToastState]);
 
   return (
-    <>
+    <div css={toast} role="alert">
       {ToastState.toastType === "error" ? (
         <Error>
           <PinkExcalmationMark width="18" height="18" />
@@ -34,7 +62,7 @@ const Alert = () => {
           {ToastState.text}
         </Success>
       )}
-    </>
+    </div>
   );
 };
 

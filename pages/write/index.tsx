e00e8e30da504ec15991368aa95dc4abed0324d1
jungsoo <sunpl13@naver.com>
2022-09-10 20:11:19 +0900

    [#8] feat : 임시저장 및 다음 이동 버튼 추가

--- pages/write/index.tsx
+++ pages/write/index.tsx
@@ -32,6 +32,12 @@ export default function Index() {
           return <Part ListLength={partIdList.length} PartNum={id} key={id} />;
         })}
       </PartSectionContainer>
+      <BtnContainer>
+        <button>임시저장</button>
+        <Link href="/write/setting">
+          <a>다음</a>
+        </Link>
+      </BtnContainer>
     </WriteContainer>
   );
 }
@@ -45,4 +51,38 @@ const WriteContainer = styled.main`
   height: 100%;
   background-clip: padding-box;
   background-color: ${Common.colors.GY50};
+`;
+
+const BtnContainer = styled.div`
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  height: 84px;
+  background-color: #fff;
+  padding: 14px 27px 0 20px;
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid ${Common.colors.GY200};
+  & button {
+    height: 36px;
+    border: 0;
+    outline: 0;
+    padding: 10px 15px;
+    ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY900 })};
+    line-height: 150%;
+    letter-spacing: -0.03em;
+    border: 1px solid ${Common.colors.GY700};
+    background-color: #fff;
+    border-radius: 5px;
+  }
+
+  & a {
+    height: 36px;
+    padding: 10px 15px;
+    background-color: ${Common.colors.BL500};
+    ${Pretendard({ font: 1.2, weight: 700, color: "#fff" })};
+    border-radius: 5px;
+    line-height: 150%;
+    letter-spacing: -0.03em;
+  }
 `;

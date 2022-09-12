4b7605a58739a0bfddb653f478f0a1494821b1f4
jungsoo <sunpl13@naver.com>
2022-09-13 01:07:44 +0900

    Add 태그 컴포넌트 추가

---
+++ components/survey/setting/AddTag.tsx
@@ -0,0 +1,108 @@
+import styled from "@emotion/styled";
+import Close from "public/icon/close.svg";
+import { Common, Pretendard } from "styles/common";
+import Link from "next/link";
+import Plus from "public/icon/plus-blue.svg";
+
+interface Iprops {
+  text: string;
+}
+
+const Chip = ({ text }: Iprops) => {
+  return (
+    <ChipStyle>
+      #{text}
+      <Close width="8" height="8" fill={Common.colors.GY500} />
+    </ChipStyle>
+  );
+};
+const AddTag = () => {
+  return (
+    <TagAdd>
+      <h1>태그를 추가해주세요</h1>
+      <span>선택 사항입니다.</span>
+      <Link href="/write/addTag">
+        <a>
+          <Plus />
+          <span className="tag-btn">태그 추가하기</span>
+        </a>
+      </Link>
+
+      <UlContainer>
+        <Chip text="설문결과가 매우 좋군여" />
+        <Chip text="태그" />
+        <Chip text="태그" />
+        <Chip text="태그" />
+        <Chip text="태그" />
+        <Chip text="태그" />
+      </UlContainer>
+    </TagAdd>
+  );
+};
+
+export default AddTag;
+
+const TagAdd = styled.section`
+  background-color: #fff;
+  padding: 0 20px;
+  padding-top: 24px;
+  padding-bottom: 32px;
+  & span {
+    ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY700 })}
+  }
+  & .tag-btn {
+    ${Pretendard({ font: 1.4, weight: 700, color: Common.colors.GY900 })}
+  }
+
+  & a {
+    margin: 18px 0;
+    display: flex;
+    align-items: center;
+
+    & svg {
+      margin-right: 6px;
+    }
+  }
+`;
+
+const UlContainer = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-wrap: nowrap;
+
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  & li {
+    flex: 0 0 auto;
+  }
+
+  & li:not(:last-of-type) {
+    margin-right: 12px;
+  }
+`;
+
+const ChipStyle = styled.li`
+  display: flex;
+  align-items: center;
+  background-color: ${Common.colors.GY50};
+  border: 1px solid ${Common.colors.GY200};
+  height: 28px;
+  width: auto;
+
+  border-radius: 90px;
+  padding: 4px 8px;
+  ${Pretendard({ font: 1.3, weight: 400, color: Common.colors.GY900 })};
+  line-height: 150%;
+  letter-spacing: -0.03em;
+  text-align: center;
+
+  & svg {
+    margin-left: 5px;
+  }
+`;

dd16465a354e287b0bfe96cdea9e56872e9c3a60
jungsoo <sunpl13@naver.com>
2022-09-13 01:04:02 +0900

    survey 타입 수정
    추가된 필드 값 추가 및 ID 값 타입 설정

--- types/survey.ts
+++ types/survey.ts
@@ -39,8 +39,14 @@ export interface ISurveyData {
   hashtag: string[]; //[해시태그 리스트,String 배열],
   sections: ISection[];
 }
-
+export type SurveyID = `SUVY${number}${string}`;
+export type SectionID = `SCTN${number}${string}`;
+export type QuestionID = `QSTN${number}${string}`;
+export type QuestionItemID = `QSTI${number}${string}`;
+export type QuestionItemListID = `SCTN${number}${string}QSTN${number}${string}`;
+export type QuestionListID = `QTSCTN${number}${string}`;
 export interface ISection {
+  id: SectionID;
   title: string; //섹션제목,
   content: string; //설명,
   nextSection: number; //*다음섹션(Integer|-1이면 이 섹션이 마지막 섹션, 사실 그냥 index값임 프론트에서도 설문 만들 때 정렬해서 보여줘야하니깐 index값 그대로 넣기),
@@ -48,17 +54,19 @@ export interface ISection {
 }
 
 export interface IQuestion {
+  id: QuestionID;
   ask: string; //*질문,
   descrip: string; //설명,
   multiFl: 0 | 1; //*(Integer|0:주관식,1:객관식),
   essFl: 0 | 1; //*필수질문여부(Integer|0:필수X,1:필수),
+  randomShowFl: 0 | 1; //"*선택지 무작위 섞기 플래그(Integer|0:무작위섞기OFF, 1:무작위섞기ON - 주관식이면 무조건 0으로 주세요"
   dupFl: 0 | 1; //*질문이 객관식일 때 중복선택 가능여부(Integer|0:중복X,1:중복가능|주관식이면 무조건 0),
   oder: number; //*순서(Integer|사실 그냥 index값임 프론트에서도 설문 만들 때 정렬해서 보여줘야하니깐 index값 그대로 넣어주세요),
   questionItems: IQuestionItem[];
 }
 
 export interface IQuestionItem {
-  id: number;
+  id: QuestionItemID;
   content: string; //*선택지,
   oder: number; //*위 oder랑 동일함,
   nextSection: number; //다음섹션(답변을 기준으로 파트이동일 때 - Integer|-1이면 설문제출, 답변 기준 파트이동이 아니라면 0으로)

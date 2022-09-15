17854566be1dda2480d209b6c5e35e34f847d1af
jungsoo <sunpl13@naver.com>
2022-09-15 15:38:35 +0900

    재사용성을 위한 Chip UI 별도 컴포넌트로 분리

---
+++ components/survey/setting/Chip.tsx
@@ -0,0 +1,44 @@
+import Close from "public/icon/close.svg";
+import { useRecoilState } from "recoil";
+import { tagState } from "states/tag";
+import { Common, Pretendard } from "styles/common";
+import styled from "@emotion/styled";
+
+interface Iprops {
+  text: string;
+}
+
+const Chip = ({ text }: Iprops) => {
+  const [TagState, setTagState] = useRecoilState(tagState);
+
+  const deleteTagHandler = () => {
+    setTagState(TagState.filter((item) => item !== text));
+  };
+  return (
+    <ChipStyle>
+      #{text}
+      <Close width="8" height="8" onClick={deleteTagHandler} fill={Common.colors.GY500} />
+    </ChipStyle>
+  );
+};
+
+export default Chip;
+
+const ChipStyle = styled.li`
+  display: flex;
+  align-items: center;
+  background-color: ${Common.colors.GY50};
+  border: 1px solid ${Common.colors.GY200};
+  height: 28px;
+  width: auto;
+
+  border-radius: 90px;
+  padding: 4px 8px;
+  ${Pretendard({ font: 1.3, weight: 400, color: Common.colors.GY900 })};
+  line-height: 150%;
+  text-align: center;
+
+  & svg {
+    margin-left: 5px;
+  }
+`;

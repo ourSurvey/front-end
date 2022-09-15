4d545496ab1677b5683792d369c20d1b6a734457
jungsoo <sunpl13@naver.com>
2022-09-15 15:42:40 +0900

    응답자 코멘트 컴포넌트 UI 생성

---
+++ components/survey/setting/CommentRespondent.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { Common, Pretendard } from "styles/common";
+const CommentRespondent = () => {
+  const [title, setTitle] = useState("설문이 종료되었습니다");
+  const [subject, setSubject] = useState("응답해주셔서 감사합니다.");
+
+  const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const onSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubject(e.target.value);
+  };
+
+  return (
+    <CommetContainer>
+      <h1>응답자에게 감사 인사를 남겨보세요.</h1>
+      <span>작성한 문구는 설문을 끝 마친 응답자에게 안내됩니다.</span>
+
+      <div>
+        <InputContainer>
+          <TitleInput className="title" type="text" onChange={onTitleChange} defaultValue={title} />
+          <div>
+            <span className="content-length">8&nbsp;</span>
+            <span> / 20자</span>
+          </div>
+        </InputContainer>
+        <InputContainer>
+          <SubjectInput onChange={onSubjectChange} defaultValue={subject} className="subject" type="text" />
+          <div>
+            <span className="content-length">8&nbsp;</span>
+            <span> / 20자</span>
+          </div>
+        </InputContainer>
+      </div>
+    </CommetContainer>
+  );
+};
+
+export default CommentRespondent;
+
+const CommetContainer = styled.section`
+  background-color: #fff;
+  padding: 0 20px;
+  padding-bottom: 32px;
+
+  & h1 {
+    padding-top: 32px;
+  }
+  & span {
+    ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY700 })}
+    line-height: 150%;
+  }
+`;
+
+const InputContainer = styled.div`
+  margin-top: 25px;
+  & input[type="text"] {
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid ${Common.colors.GY200};
+    padding-bottom: 8px;
+    outline-style: none;
+  }
+  & div {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 5px;
+    padding-right: 5px;
+  }
+  & span {
+    ${Pretendard({ font: 1, weight: 400, color: Common.colors.GY500 })};
+  }
+  & .content-length {
+    ${Pretendard({ font: 1, weight: 700, color: Common.colors.GY700 })};
+  }
+`;
+
+const TitleInput = styled.input`
+  ${Pretendard({ font: 2, weight: 700, color: "#000" })};
+  line-height: 23px;
+`;
+const SubjectInput = styled.input`
+  ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY900 })};
+  line-height: 24px;
+`;

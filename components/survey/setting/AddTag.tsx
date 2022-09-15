05db786334124c1e8dff25509ddd52f145ec9829
jungsoo <sunpl13@naver.com>
2022-09-15 15:40:08 +0900

    재사용성을 위한 Chip 컴포넌트 분리

--- components/survey/setting/AddTag.tsx
+++ components/survey/setting/AddTag.tsx
@@ -1,21 +1,8 @@
 import styled from "@emotion/styled";
-import Close from "public/icon/close.svg";
 import { Common, Pretendard } from "styles/common";
 import Link from "next/link";
 import Plus from "public/icon/plus-blue.svg";
 
-interface Iprops {
-  text: string;
-}
-
-const Chip = ({ text }: Iprops) => {
-  return (
-    <ChipStyle>
-      #{text}
-      <Close width="8" height="8" fill={Common.colors.GY500} />
-    </ChipStyle>
-  );
-};
 const AddTag = () => {
   return (
     <TagAdd>
@@ -84,24 +71,5 @@ const UlContainer = styled.ul`
 
   & li:not(:last-of-type) {
     margin-right: 12px;
-  }
-`;
-
-const ChipStyle = styled.li`
-  display: flex;
-  align-items: center;
-  background-color: ${Common.colors.GY50};
-  border: 1px solid ${Common.colors.GY200};
-  height: 28px;
-  width: auto;
-
-  border-radius: 90px;
-  padding: 4px 8px;
-  ${Pretendard({ font: 1.3, weight: 400, color: Common.colors.GY900 })};
-  line-height: 150%;
-  text-align: center;
-
-  & svg {
-    margin-left: 5px;
   }
 `;

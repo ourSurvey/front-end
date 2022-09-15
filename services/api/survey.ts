3f72bb7197b26e5a79af7d9d7b37ba0992384a79
jungsoo <sunpl13@naver.com>
2022-09-15 15:49:12 +0900

    태그 검색 api

--- services/api/survey.ts
+++ services/api/survey.ts
@@ -1,5 +1,7 @@
 import gql from "graphql-tag";
+import ApiClient from "services/ApiClient";
 
+const api = new ApiClient();
 export const GET_SURVEY = gql`
   query ($page: Int!, $size: Int!) {
     getSurveyToPage(page: $page, size: $size) {
@@ -25,3 +27,5 @@ export const GET_SURVEY = gql`
     }
   }
 `;
+
+export const getTagList = (tagString: string) => api.get(`/hashtag/${tagString}`);

6bdc472d5b114e0eaa6ba48ec8f37cf3e23a4bde
jungsoo <sunpl13@naver.com>
2022-09-23 15:27:42 +0900

    서베이 생성 시 id값 제거한 객체 리턴해주는 함수 생성
    생성 시에 id 값이 필요 없기 때문에 삭제해주는 함수 필요

---
+++ utills/deleteIdProperty.ts
@@ -0,0 +1,19 @@
+import { ISurveyData } from "types/survey";
+export const deleteIDproperty = (object: ISurveyData) => {
+  const sectionArr = [...object.sections];
+  const newArr = sectionArr.map((item) => {
+    const questionList = item.questions.map((qusetion) => {
+      const questionItemList = qusetion.questionItems.map((questionItem) => {
+        const { id, ...questionItemProperties } = questionItem;
+        return questionItemProperties;
+      });
+
+      const { id, ...questionProperties } = qusetion;
+      return { ...questionProperties, questionItems: questionItemList };
+    });
+
+    const { id, ...sectionProperties } = item;
+    return { ...sectionProperties, questions: questionList };
+  });
+  return { ...object, sections: newArr };
+};

83be7ca1b44734c1b07f27abe45cff24c27e79cd
jungsoo <sunpl13@naver.com>
2022-10-13 15:05:53 +0900

    [#15] style : 디자인 시안대로 달력 UI 수정

--- components/modal/DatePickerModal.tsx
+++ components/modal/DatePickerModal.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 import ko from 'date-fns/locale/ko';
 import { addDays } from 'date-fns';
-import { Pretendard, Common, SpaceBetween } from 'styles/common';
+import { Pretendard, Common, SpaceBetween, AlignAndJustifyCenter } from 'styles/common';
 import { DateRange } from 'react-date-range';
-import { useState } from 'react';
+import { createElement, useEffect, useState } from 'react';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
@@ -11,11 +11,40 @@ export interface RangeWithKey extends Range {
   key: 'selection';
 }
 
+interface IStyle {
+  width: number;
+}
+
 export type OnChangeProps = Range | { selection: RangeWithKey } | Date;
 
 const DatePickerModal = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [dayWidth, setdayWidth] = useState(0);
+
+  const compareTime = (startDate: Date, endDate: Date) => {
+    const timeMs = endDate.getTime() - startDate.getTime();
+    return timeMs / 1000 / 60 / 60 / 24; //일로 변경
+  };
+
+  useEffect(() => {
+    const width = document.getElementsByClassName('rdrDay rdrDayToday')[0].clientWidth;
+    setdayWidth(width);
+
+    const rdrInRangeList = document.getElementsByClassName('rdrInRange');
+    // if (compareTime(startDate, endDate) === 1) {
+    //   const rdrStartEdge = document.getElementsByClassName('rdrStartEdge')[0];
+    //   const rdrEndEdge = document.getElementsByClassName('rdrEndEdge')[0];
+
+    //   // rdrStartEdge.classList.toggle('rightRange');
+    //   rdrEndEdge.classList.toggle('leftRange');
+    // }
+
+    if (rdrInRangeList.length > 0) {
+      rdrInRangeList[0].classList.toggle('leftRange');
+      rdrInRangeList[rdrInRangeList.length - 1].classList.toggle('rightRange');
+    }
+  }, [startDate, endDate]);
 
   const selectRagne = {
     startDate: startDate,
@@ -29,11 +58,11 @@ const DatePickerModal = () => {
   };
 
   return (
-    <DatePickerContainer>
+    <DatePickerContainer width={dayWidth}>
       <h1>설문 진행기간을 선택해주세요</h1>
-      <div>
+      <DateWrapper>
         <DateRange
-          minDate={addDays(new Date(), -30)}
+          minDate={addDays(new Date(), 0)}
           maxDate={addDays(new Date(), 120)}
           direction="vertical"
           ranges={[selectRagne]}
@@ -50,14 +79,14 @@ const DatePickerModal = () => {
           showMonthAndYearPickers={false}
           monthDisplayFormat={'yyyy.MM'}
         />
-      </div>
+      </DateWrapper>
     </DatePickerContainer>
   );
 };
 
 export default DatePickerModal;
 
-const DatePickerContainer = styled.div`
+const DatePickerContainer = styled.div<IStyle>`
   & h1 {
     text-align: center;
     ${Pretendard({ font: 1.6, weight: 700, color: Common.colors.GY900 })}
@@ -65,6 +94,14 @@ const DatePickerContainer = styled.div`
 
   & .rdrDayNumber {
     ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY700 })}
+  }
+
+  & .rdrDay {
+    position: relative;
+    ${AlignAndJustifyCenter()};
+  }
+  & .rdrDayNumber {
+    z-index: 2;
   }
 
   & .rdrMonthAndYearWrapper {
@@ -77,15 +114,63 @@ const DatePickerContainer = styled.div`
     }
   }
 
+  &.rdrCalendarWrapper,
+  .rdrDateRangeWrapper {
+    width: 100%;
+  }
+
+  & .rdrMonth {
+    width: 100%;
+  }
+
+  & .rdrDayEndOfWeek .rdrInRange {
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
+  & .rdrDayStartOfWeek .rdrInRange {
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+
   & .rdrMonthName {
     text-align: center;
     ${Pretendard({ font: 1.2, weight: 700, color: Common.colors.GY900 })};
     line-height: 150%;
   }
 
+  & .leftRange::before {
+    content: '';
+    width: ${(props) => props.width / 2}px;
+    height: 26px;
+    position: absolute;
+    top: 0;
+    bottom: 5px;
+    left: -${(props) => props.width / 2}px;
+    background-color: ${Common.colors.BL50};
+    z-index: 0;
+  }
+  & .rightRange::after {
+    content: '';
+    width: ${(props) => props.width / 2}px;
+    height: 26px;
+    position: absolute;
+    top: 0;
+    bottom: 5px;
+    right: -${(props) => props.width / 2}px;
+    background-color: ${Common.colors.BL50};
+    z-index: 0;
+  }
+
   & .rdrStartEdge,
   .rdrEndEdge {
     background-color: ${Common.colors.GY900};
+    border-radius: 50%;
+    width: 26px;
+    height: 26px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
   }
   & .rdrDay:not(.rdrDayPassive) .rdrStartEdge ~ .rdrDayNumber span,
   .rdrDay:not(.rdrDayPassive) .rdrEndEdge ~ .rdrDayNumber span {
@@ -95,4 +180,10 @@ const DatePickerContainer = styled.div`
   .rdrDay:not(.rdrDayPassive) .rdrSelected ~ .rdrDayNumber span {
     color: ${Common.colors.GY700};
   }
+`;
+
+const DateWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;

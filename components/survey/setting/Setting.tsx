eae6a6242e88229e7544de6902b362f7370bc233
jungsoo <sunpl13@naver.com>
2022-10-19 20:51:28 +0900

    [#8] refactor: setting 페이지 업로드 부분 리렌더 이슈로 인한 의존성 분리
    컴포넌트 분리

---
+++ components/survey/setting/Setting.tsx
@@ -0,0 +1,148 @@
+import { memo } from 'react';
+import styled from '@emotion/styled';
+import { Common, Pretendard, SpaceBetween } from 'styles/common';
+import { Button } from 'components/common/Button';
+import PeriodSettingWrapper from 'components/survey/setting/PeriodSettingWrapper';
+import TImeTaken from 'components/survey/setting/TImeTaken';
+import AddTag from 'components/survey/setting/AddTag';
+import ShareResult from 'components/survey/setting/ShareResult';
+import CommentRespondent from 'components/survey/setting/CommentRespondent';
+import CreateSurveyHeader from 'components/survey/CreateSurveyHeader';
+import { useHeaderScroll } from 'hooks/useHeaderScroll';
+interface IProps {
+  setShowModal: (bool: boolean) => void;
+  closinTitle: string;
+  setClosinTitle: (text: string) => void;
+  closingComment: string;
+  setclosingComment: (text: string) => void;
+  temporaryStorageHandler: () => void;
+}
+
+const Setting = ({
+  setShowModal,
+  closinTitle,
+  setClosinTitle,
+  setclosingComment,
+  closingComment,
+  temporaryStorageHandler,
+}: IProps) => {
+  const { hide, scrollDetectHandler } = useHeaderScroll();
+
+  return (
+    <SettingPage>
+      <HeaderWrap className={!hide ? 'hide' : ''}>
+        <CreateSurveyHeader name="설정" hasUnderLine={true} step="03" />
+      </HeaderWrap>
+      <SettingItemContainer onScroll={scrollDetectHandler}>
+        <PeriodSettingWrapper />
+        <TImeTaken />
+        <AddTag />
+        <ShareResult />
+        <CommentRespondent
+          closinTitle={closinTitle}
+          closingComment={closingComment}
+          setClosinTitle={setClosinTitle}
+          setclosingComment={setclosingComment}
+        />
+        <BtnContainer>
+          <Button
+            fontFamily="pretendard"
+            fontSize={1.2}
+            fontWeight={400}
+            textColor={Common.colors.GY900}
+            isDisabled={false}
+            color="transparent"
+            wUnit="%"
+            width={33}
+            btnText="임시저장"
+            className="temporary-storage"
+            onClick={temporaryStorageHandler}
+          />
+
+          <Button
+            fontFamily="pretendard"
+            fontSize={1.2}
+            fontWeight={700}
+            textColor="#fff"
+            isDisabled={false}
+            color={Common.colors.BL500}
+            btnText="설문 업로드"
+            className="upload"
+            onClick={() => setShowModal(true)}
+          />
+        </BtnContainer>
+      </SettingItemContainer>
+    </SettingPage>
+  );
+};
+
+export default memo(Setting);
+
+const SettingPage = styled.main`
+  height: 100%;
+  background-color: ${Common.colors.GY50};
+  & h1 {
+    ${Pretendard({ font: 1.6, weight: 700, color: Common.colors.GY900 })};
+    margin: 0;
+    line-height: 150%;
+  }
+
+  & section {
+    margin-bottom: 10px;
+
+    &:last-of-type {
+      margin-bottom: 1px;
+    }
+  }
+`;
+
+const SettingItemContainer = styled.div`
+  display: block;
+  height: calc(100% - 54px) !important;
+  width: 100%;
+  padding-top: 61.5px;
+
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const BtnContainer = styled.footer`
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  ${SpaceBetween()};
+  border-top: 1px solid ${Common.colors.GY200};
+  background-color: #fff;
+  padding: 13px 20px 34px 20px;
+  & button {
+    outline: none;
+    border: none;
+    border-radius: 5px;
+
+    height: 36px;
+    line-height: 150%;
+  }
+
+  & .temporary-storage {
+    width: 33%;
+    border: 1px solid ${Common.colors.GY700};
+  }
+  & .upload {
+    width: calc(67% - 7px);
+    background-color: ${Common.colors.BL500};
+  }
+`;
+const HeaderWrap = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  transition: 0.4s ease;
+
+  &.hide {
+    transform: translateY(-60.5px);
+  }
+`;
---
+++ components/survey/setting/SettingWrapper.tsx
@@ -0,0 +1,164 @@
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import { getMyPoint } from 'services/api/point';
+import { useMutation } from 'react-query';
+import { Common, Pretendard } from 'styles/common';
+import { createSurvey } from 'services/api/survey';
+import { deleteIDproperty } from 'utills/deleteIdProperty';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { toastState } from 'states/modal';
+import { surveySelector } from 'states/survey';
+import styled from '@emotion/styled';
+import Setting from './Setting';
+import Portal from 'components/common/Portal';
+import ModalTemplate from 'components/modal/ModalTemplate';
+import SurveyUpLoadAlert from 'components/modal/SurveyUpLoadAlert';
+import LackPointModal from 'components/modal/LackPointModal';
+import SurveySkeleton from 'components/skeleton/SurveySkeleton';
+
+const Placeholder: React.FC = () => (
+  <ItemContainer>
+    <DateContainer>
+      <SurveySkeleton width={200} height={25} rounded />
+    </DateContainer>
+    <DateContainer>
+      <SurveySkeleton width={100} wUnit="%" height={36} />
+    </DateContainer>
+    <SurveySkeleton width={100} wUnit="%" height={36} />
+  </ItemContainer>
+);
+
+const SettingWrapper = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [closinTitle, setClosinTitle] = useState('설문이 종료되었습니다');
+  const [closingComment, setclosingComment] = useState('응답해주셔서 감사합니다.');
+  const setToastState = useSetRecoilState(toastState);
+  const state = useRecoilValue(surveySelector);
+  const { isLoading, data } = useQuery(['point'], () => getMyPoint(), {
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+  });
+
+  const createSurveyHandler = useMutation(createSurvey, {
+    onSuccess: (data) => {
+      if (data.status === 200) {
+        setToastState({
+          text: '설문이 정상적으로 생성되었습니다!',
+          toastType: 'success',
+          visible: true,
+        });
+      }
+    },
+    onError: (data: any) => {
+      setToastState({
+        text: data.response?.data.message,
+        toastType: 'error',
+        visible: true,
+      });
+    },
+  });
+
+  const createSurveyButton = () => {
+    const sendState = deleteIDproperty(state);
+    createSurveyHandler.mutate({ ...sendState, id: '', closingComment: `${closinTitle}|${closingComment}`, tempFl: 0 });
+  };
+
+  const temporarySurveyHandler = useMutation(createSurvey, {
+    onSuccess: (data) => {
+      if (data.status === 200) {
+        setToastState({
+          text: '임시저장 되었습니다.',
+          toastType: 'success',
+          visible: true,
+        });
+      }
+    },
+    onError: (data: any) => {
+      setToastState({
+        text: data.response?.data.message,
+        toastType: 'error',
+        visible: true,
+      });
+    },
+  });
+
+  const temporaryStorageHandler = () => {
+    const sendState = deleteIDproperty(state);
+    temporarySurveyHandler.mutate({
+      ...sendState,
+      id: '',
+      closingComment: `${closinTitle}|${closingComment}`,
+      tempFl: 1,
+    });
+  };
+
+  if (isLoading) {
+    return (
+      <SettingPage>
+        <div css={{ marginTop: '64px' }}>
+          <Placeholder />
+          <Placeholder />
+          <Placeholder />
+          <Placeholder />
+          <Placeholder />
+        </div>
+      </SettingPage>
+    );
+  }
+
+  return (
+    <>
+      <Setting
+        setShowModal={setShowModal}
+        closinTitle={closinTitle}
+        closingComment={closingComment}
+        temporaryStorageHandler={temporaryStorageHandler}
+        setclosingComment={setclosingComment}
+        setClosinTitle={setClosinTitle}
+      />
+      <Portal selector="#portal">
+        {data.data < 500 ? (
+          <ModalTemplate visibleState={showModal} setVisible={setShowModal} height={50}>
+            <LackPointModal setVisible={setShowModal} point={data.data} />
+          </ModalTemplate>
+        ) : (
+          <ModalTemplate visibleState={showModal} setVisible={setShowModal} height={25}>
+            <SurveyUpLoadAlert setVisible={setShowModal} point={data.data} upload={createSurveyButton} />
+          </ModalTemplate>
+        )}
+      </Portal>
+    </>
+  );
+};
+
+export default SettingWrapper;
+
+const SettingPage = styled.main`
+  height: 100%;
+  background-color: ${Common.colors.GY50};
+  & h1 {
+    ${Pretendard({ font: 1.6, weight: 700, color: Common.colors.GY900 })};
+    margin: 0;
+    line-height: 150%;
+  }
+
+  & section {
+    margin-bottom: 10px;
+
+    &:last-of-type {
+      margin-bottom: 1px;
+    }
+  }
+`;
+
+const ItemContainer = styled.div`
+  background-color: #fff;
+  padding: 15px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.16);
+  border-radius: 5px;
+  margin-bottom: 24px;
+`;
+
+const DateContainer = styled.div`
+  margin-bottom: 10px;
+`;

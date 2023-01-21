2dc8b5a322ba37dfd66e2c6a3789ac9bef01dff6
jungsoo <sunpl13@naver.com>
2023-01-21 22:28:22 +0900

    [#19] style : 나의 설문 스켈레톤 UI 설정

--- components/mySurvey/Written.tsx
+++ components/mySurvey/Written.tsx
@@ -1,8 +1,61 @@
-import React from 'react';
+import { useQueries, useQuery } from 'react-query';
+import { getMySurveies, isHaveSurveyTemp } from 'services/api/survey';
+import SurveySkeleton from 'components/skeleton/SurveySkeleton';
+import styled from '@emotion/styled';
+import { AlignAndJustifyCenter } from 'styles/common';
+const Placeholder: React.FC = () => (
+  <ItemContainer className="survey-items">
+    <UlContainer>
+      <SurveySkeleton width={100} height={15} />
+    </UlContainer>
+    <SurveySkeleton width={200} height={25} rounded />
+    <Content></Content>
+    <DateContainer>
+      <SurveySkeleton width={120} height={14} rounded />
+    </DateContainer>
+    <UlContainer>
+      <SurveySkeleton width={49} wUnit="%" height={36} />
+      &nbsp;
+      <SurveySkeleton width={49} wUnit="%" height={36} />
+    </UlContainer>
+  </ItemContainer>
+);
+
+const Buttons: React.FC = () => (
+  <>
+    <ButtonsContainer>
+      <SurveySkeleton width={15} wUnit="%" height={26} rounded />
+      &nbsp;
+      <SurveySkeleton width={15} wUnit="%" height={26} rounded />
+      &nbsp;
+      <SurveySkeleton width={15} wUnit="%" height={26} rounded />
+      &nbsp;
+      <SurveySkeleton width={15} wUnit="%" height={26} rounded />
+    </ButtonsContainer>
+  </>
+);
 
 type Props = {};
 
 const Written = (props: Props) => {
+  const result = useQueries([
+    { queryKey: ['temps'], queryFn: () => isHaveSurveyTemp(), suspense: true },
+    { queryKey: ['mySurveies'], queryFn: () => getMySurveies(), suspense: true },
+  ]);
+
+  console.log(result);
+
+  if (result[0].isLoading || result[1].isLoading) {
+    return (
+      <SkeletonContainer>
+        <Buttons />
+        <Placeholder />
+        <Placeholder />
+        <Placeholder />
+      </SkeletonContainer>
+    );
+  }
+
   return (
     <article className="written" role="tabpanel">
       Written
@@ -11,3 +64,43 @@ const Written = (props: Props) => {
 };
 
 export default Written;
+
+const SkeletonContainer = styled.div`
+  padding: 0 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ItemContainer = styled.div`
+  padding: 15px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.16);
+  border-radius: 14px;
+  margin-bottom: 24px;
+  width: 100%;
+`;
+
+const UlContainer = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  margin-top: 0px;
+  padding: 0;
+`;
+
+const ButtonsContainer = styled(UlContainer)`
+  width: 100%;
+`;
+
+const Content = styled.p`
+  & span {
+    margin-top: 5px;
+
+    &:last-child {
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+const DateContainer = styled.div`
+  margin-bottom: 10px;
+`;

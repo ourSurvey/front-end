e97c1f2aef5ebc0aad48eb11134011342377f0ab
jungsoo <sunpl13@naver.com>
2023-03-07 17:09:07 +0900

    [#19] refactor: 재사용성을 위한 SurveyItem 컴포넌트 합성컴포넌트로 분리
    # Conflicts:
    #	components/survey/SurveyItem.tsx

---
+++ components/survey/SurveyItems.tsx
@@ -0,0 +1,115 @@
+import { ReactNode } from 'react';
+import styled from '@emotion/styled';
+import Link from 'next/link';
+import { Common, Pretendard } from 'styles/common';
+
+interface IStyle {
+  hashtagList: [] | null;
+}
+
+interface IChildren {
+  children?: ReactNode;
+}
+interface ILink extends IChildren {
+  url: string;
+}
+
+interface IHashTags {
+  hashtagList: null | [];
+}
+
+interface ISurveyItemMain extends IChildren {
+  ref?: React.RefObject<HTMLDivElement>;
+}
+
+export const Labels = ({ children }: IChildren) => <UlContainer>{children}</UlContainer>;
+export const Subject = ({ children }: IChildren) => <SubjectStyle>{children}</SubjectStyle>;
+export const Contents = ({ children }: IChildren) => <Content>{children}</Content>;
+export const Periods = ({ children }: IChildren) => <DateContainer>{children}</DateContainer>;
+export const LinkButton = ({ children, url }: ILink) => (
+  <Link href={url}>
+    <a>{children}</a>
+  </Link>
+);
+export const HashTags = ({ hashtagList }: IHashTags) => (
+  <>
+    {hashtagList && hashtagList.length > 0 ? (
+      <Hashtag hashtagList={hashtagList}>{hashtagList?.map((item) => `#${item}`)}</Hashtag>
+    ) : null}
+  </>
+);
+
+const SurveyItemMain = ({ children }: ISurveyItemMain) => {
+  return <ItemContainer>{children}</ItemContainer>;
+};
+
+export const SurveyItem = Object.assign(SurveyItemMain, {
+  Labels,
+  Subject,
+  Contents,
+  Periods,
+  LinkButton,
+  HashTags,
+});
+
+const ItemContainer = styled.div`
+  padding: 15px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.16);
+  border-radius: 14px;
+  margin-bottom: 24px;
+
+  width: 100%;
+  z-index: 20;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+const SubjectStyle = styled.h1`
+  ${Pretendard({ font: 1.6, weight: 700, color: Common.colors.GY900 })}
+  line-height: 150%;
+  overflow: hidden;
+  white-space: normal;
+  display: -webkit-box;
+  text-overflow: ellipsis;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+`;
+
+const UlContainer = styled.ul`
+  display: flex;
+  margin-bottom: 10px;
+  list-style-type: none;
+  margin-top: 0px;
+  padding: 0;
+`;
+
+const Content = styled.p`
+  ${Pretendard({ font: 1.2, weight: 400, color: Common.colors.GY700 })}
+  line-height: 150%;
+
+  text-overflow: ellipsis;
+  overflow: hidden;
+  -webkit-line-clamp: 2;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+`;
+
+const DateContainer = styled.span`
+  margin-bottom: 10px;
+  display: block;
+  & span {
+    ${Pretendard({ font: 1, weight: 700, color: Common.colors.GY500 })}
+  }
+  ${Pretendard({ font: 1, weight: 400, color: Common.colors.GY500 })}
+`;
+
+const Hashtag = styled.span<IStyle>`
+  width: 100%;
+  display: -webkit-box;
+  ${Pretendard({ font: 1, weight: 700, color: Common.colors.GY500 })};
+  line-height: 150%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  margin-top: ${({ hashtagList }) => (hashtagList && hashtagList.length > 0 ? '10px' : '0')};
+`;
---
+++ components/survey/SurveyListItem.tsx
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled';
+import { Common, Pretendard, AlignAndJustifyCenter } from 'styles/common';
+import { IContent } from 'types/survey';
+import Lighting from 'public/icon/lighting.svg';
+import { useRef } from 'react';
+import { Button } from 'components/common/Button';
+import { SurveyItem } from './SurveyItems';
+
+const SurveyListItem = (props: IContent) => {
+  const { subject, content, openFl, minute, startDate, endDate, hashtagList, id } = props;
+
+  const target = useRef(null); // 대상 ref
+  return (
+    <SurveyItem ref={target}>
+      <SurveyItem.Labels>
+        <>
+          {minute <= 5 ? (
+            <FastChip>
+              <Lighting width="17" height="19" />
+              예상 시간: {minute}분
+            </FastChip>
+          ) : (
+            <LiStyle>예상 시간: {minute}분</LiStyle>
+          )}
+          {openFl === 1 ? <LiStyle>설문 결과 공개</LiStyle> : null}{' '}
+        </>
+      </SurveyItem.Labels>
+      <SurveyItem.Subject>{subject}</SurveyItem.Subject>
+      <SurveyItem.Contents>{content}</SurveyItem.Contents>
+      <SurveyItem.Periods>
+        <>
+          <span>설문 기간 </span>
+          {startDate} ~ {endDate}
+        </>
+      </SurveyItem.Periods>
+      <SurveyItem.LinkButton key={id} url={`/survey/${id}`}>
+        <Button
+          fontSize={1.2}
+          fontWeight={700}
+          textColor="#fff"
+          color={Common.colors.BL500}
+          isDisabled={false}
+          btnText="설문 참여하기"
+          height={36}
+          hUnit="px"
+          fontFamily="pretendard"
+        />
+      </SurveyItem.LinkButton>
+      <SurveyItem.HashTags hashtagList={hashtagList}></SurveyItem.HashTags>
+    </SurveyItem>
+  );
+};
+
+export default SurveyListItem;
+
+const LiStyle = styled.li`
+  ${AlignAndJustifyCenter()};
+  margin-right: 5px;
+  padding: 0 4px;
+  border-radius: 3px;
+  border-radius: 3px;
+  background-color: ${Common.colors.GY50};
+  ${Pretendard({ font: 1, weight: 400, color: '#000' })}
+  line-height: 150%;
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+const FastChip = styled.li`
+  ${AlignAndJustifyCenter()};
+  border-radius: 3px;
+  margin-right: 5px;
+  padding: 0 4px 0 0;
+  background-color: ${Common.colors.GY700} !important;
+  ${Pretendard({ font: 1, weight: 700, color: '#fff' })}
+  line-height: 150%;
+`;
